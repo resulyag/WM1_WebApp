4b51d7785e8d909d4206bc797975dc36dba8f33e
resulyag <resulyag@gmail.com>
2021-12-13 17:44:49 +0300

    Products Controller and Index View çalışması

--- Ilk_Mvc_Pojesi/Controllers/ProductsController.cs
+++ Ilk_Mvc_Pojesi/Controllers/ProductsController.cs
@@ -15,14 +15,29 @@ namespace Ilk_Mvc_Pojesi.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        private int _pageSize = 10;
+        public IActionResult Index(int? page = 1)
         {
-            var model = _context.Products.Include(x=>x.Category).ToList();
+            var model = _context.Products
+                .Include(x => x.Category)
+                .Include(x => x.Supplier)
+                .OrderBy(x => x.Category.CategoryName)
+                .ThenBy(x => x.ProductName)
+                .Skip((page.GetValueOrDefault() - 1) * _pageSize )
+                .Take(_pageSize)
+                .ToList();
+            //ViewBag.Categories = _context.Categories.OrderBy(x => x.CategoryName).ToList();
+            //ViewBag.Suppliers = _context.Suppliers.OrderBy(x => x.CompanyName).ToList();
+            ViewBag.Page = page.GetValueOrDefault(1);
+            ViewBag.Limit =(int)Math.Ceiling( _context.Products.Count() /(double) _pageSize);
             return View(model);
         }
         public IActionResult Detail(int? id)
         {
-            var model = _context.Products.Include(x=>x.Category).FirstOrDefault(x => x.ProductId == id);
+            var model = _context.Products
+                .Include(x=>x.Category)
+                .Include(x=>x.Supplier)
+                .FirstOrDefault(x => x.ProductId == id);
             return View(model);
         }
 
--- Ilk_Mvc_Pojesi/Models/NorthwindContext.cs
+++ Ilk_Mvc_Pojesi/Models/NorthwindContext.cs
@@ -56,7 +56,7 @@ namespace Ilk_Mvc_Pojesi.Models
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=LAPTOP-4NQEV989;Initial Catalog=Northwind;Integrated Security=True");
             }
-        }
+        } 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
