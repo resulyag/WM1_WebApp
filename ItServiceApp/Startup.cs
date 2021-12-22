4fe87668ba8c946ff7a7e8c347a1ab9ef29a3b4b
resulyag <resulyag@gmail.com>
2021-12-22 21:01:59 +0300

    registerviewmodel

--- ItServiceApp/Startup.cs
+++ ItServiceApp/Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ItServiceApp.Data;
+using ItServiceApp.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +16,47 @@ namespace ItServiceApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MyContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"));
+            });
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+             {
+                 options.Password.RequireUppercase = false;
+                 options.Password.RequireLowercase = false;
+                 options.Password.RequireDigit = true;
+                 options.Password.RequiredLength = 5;
+
+                 // Lockout settings
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                 options.Lockout.MaxFailedAccessAttempts = 3;
+                 options.Lockout.AllowedForNewUsers = false;
+
+                 // User settings
+                 options.User.AllowedUserNameCharacters = "";
+                 options.User.RequireUniqueEmail = true;
+             });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+
             services.AddControllersWithViews();
         }
 
