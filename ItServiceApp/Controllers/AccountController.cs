7e7e1cc2c6816a397a905aa40aebd42ad16766bf
resulyag <resulyag@gmail.com>
2021-12-23 17:30:43 +0300

    Register post action ok

--- ItServiceApp/Controllers/AccountController.cs
+++ ItServiceApp/Controllers/AccountController.cs
@@ -1,16 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using ItServiceApp.Models.Identity;
+using ItServiceApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace ItServiceApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Bu kullanıcı adı daha önce kayıt edilmiştir");
+                return View(model);
+            }
+
+            user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.Email), "Bu email adresi daha önce kayıt edilmiştir");
+                return View(model);
+            }
+
+            user = new ApplicationUser()
+            {
+                Email = model.Email,
+                Name = model.Name,
+                UserName = model.UserName,
+                Surname = model.Surname
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                //kullanıcıya rol atama
+                //email onay maili
+                //login sayfasına yönlendirme
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Bir hata oluştu");
+                return View(model);
+            }
         }
     }
 }
