89859f1bb94addb3ffeca09963b863ab604ea818
resulyag <resulyag@gmail.com>
2022-02-01 17:17:21 +0300

    tablo edit update tamamlandı

--- ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
+++ ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
@@ -1,9 +1,14 @@
 ﻿using DevExtreme.AspNet.Data;
 using ItServiceApp.Extensions;
 using ItServiceApp.Models.Identity;
+using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ItServiceApp.Areas.Admin.Controllers
 {
@@ -23,6 +28,25 @@ namespace ItServiceApp.Areas.Admin.Controllers
         {
             var data = _userManager.Users;
             return Ok(DataSourceLoader.Load(data,loadOptions));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(string key, string values)
+        {
+            var data = _userManager.Users.FirstOrDefault(x=>x.Id == key);
+            if (data == null)
+                return StatusCode(StatusCodes.Status409Conflict, new JsonResponseViewModel()
+                {
+                    IsSuccess = false,
+                    ErrorMessage = "Kullanıcı Bulunamadı"
+                });
+
+            JsonConvert.PopulateObject(values, data);
+            if(!TryValidateModel(data))
+                return BadRequest(ModelState.ToFullErrorString());
+
+            var result = await _userManager.UpdateAsync(data);  
+            return Ok(new JsonResponseViewModel());
         }
     }
 }
