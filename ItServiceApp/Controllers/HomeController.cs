c631c6a63cacbe7652bbe53f5de7924596d1c71c
resulyag <resulyag@gmail.com>
2022-02-02 20:41:15 +0300

    PricingTableViewModel OK

--- ItServiceApp/Controllers/HomeController.cs
+++ ItServiceApp/Controllers/HomeController.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ItServiceApp.Data;
+using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ItServiceApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyContext _dbContext;
+
+        public HomeController(MyContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<PricingTableViewModel> result = new List<PricingTableViewModel>();
+
+            var data = _dbContext.SubscriptionTypes.ToList();
+            
+            foreach (var item in data)
+            {
+                result.Add(new PricingTableViewModel
+                {
+                    PricingName = item.Name,
+                    PricingDescription = item.Description,
+                    PricingMonth = item.Month,
+                    PricingPrice = item.Price
+                });
+            }
+            return View(result);
         }
     }
 }
---
+++ ItServiceApp/ViewModels/PricingTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ItServiceApp.ViewModels
+{
+    public class PricingTableViewModel
+    {
+        public string PricingName { get; set; }
+        public decimal PricingPrice { get; set; }
+        public string PricingDescription { get; set; }
+        public int PricingMonth { get; set; }
+    }
+}
